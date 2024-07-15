61bb6999b60b2668b08bbc988c3e7c5a61c4dd5f
nimaLinux <lamap8708@gmail.com>
2024-07-15 13:28:57 +0545

    green

--- src/components/SisterOrg.jsx
+++ src/components/SisterOrg.jsx
@@ -7,15 +7,19 @@ const SisterOrg = () => {
       <div className="sis ">
         <div className="container">
           <div className="row py-4  d-flex align-items-center justify-content-center">
+            <div className="col-md-3 p-md-0">
+              {/* <img src="/bgchild22.png" alt="" className="img-fluid " /> */}
+              <img
+                src="/school/school4.JPG"
+                alt=""
+                className="img-fluid  rounded-2"
+              />
+            </div>
 
-          <div className="col-md-3 p-md-0">
-          {/* <img src="/bgchild22.png" alt="" className="img-fluid " /> */}
-          <img src="/school/school4.JPG" alt=""  className="img-fluid  rounded-2"/>
-
-          </div>
-        
             <div className="col-md-6 ">
-            <h2 className="text-center     border-bottom-title w-100 head-color"><span className="addcolor">Sister</span> Organization</h2>
+              <h2 className="text-center     border-bottom-title w-100 head-color">
+                <span className="addcolor">Sister</span> Organization
+              </h2>
               <div className="poster">
                 <img
                   src="/sisremove.png"
@@ -30,9 +34,12 @@ const SisterOrg = () => {
               </div>
             </div>
             <div className="col-md-3 p-md-0">
-            {/* <img src="/bgchild33.png" alt="" className="img-fluid " /> */}
-            <img src="/smallchild.jpg" alt=""   className="img-fluid rounded-2" />
-
+              {/* <img src="/bgchild33.png" alt="" className="img-fluid " /> */}
+              <img
+                src="/smallchild.jpg"
+                alt=""
+                className="img-fluid rounded-2"
+              />
             </div>
           </div>
         </div>
